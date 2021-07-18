eea30e69d187aec3fd981e1c33a861320e2da5d6
VVIS <robertjwisniowski@gmail.com>
2021-07-18 18:04:15 +0200

    Respawn has been added.

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
@@ -19,8 +20,14 @@ public class CollisionHandler : MonoBehaviour
                 break;
 
             default:
-                Debug.Log("Katastrofa");
+                ReloadLevel();
                 break;
         }
+    }
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        //It assures that current scene will be realoaded.
+        SceneManager.LoadScene(currentSceneIndex);
     }
 }
