2d593da9223e1194be050e40d60fe7616e96d655
VVIS <robertjwisniowski@gmail.com>
2021-07-21 18:16:50 +0200

    Move.cs script has been refactored.

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -42,39 +42,23 @@ public class Move : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
-            if (!audioSource.isPlaying)
-            {
-                audioSource.PlayOneShot(mainEngine);
-            }
-            if (!rocketJetParticle.isPlaying)
-            {
-                rocketJetParticle.Play();
-            }
+            TurnOnMainEngine();
         }
         else
         {
-            rocketJetParticle.Stop();
-            audioSource.Stop();
+            TurnOffMainEngine();
         }
     }
+
     void ProcessRotation()
     {
         if (Input.GetKey(KeyCode.A))
         {
-            ApplyRotation(rotationThrust);
-            if (!leftThrustleParticle.isPlaying)
-            {
-                leftThrustleParticle.Play();
-            }
+            TurnLeft();
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            ApplyRotation(-rotationThrust);
-            if (!rightThrustleParticle.isPlaying)
-            {
-                rightThrustleParticle.Play();
-            }
+            TurnRight();
         }
         else
         {
@@ -84,10 +68,50 @@ public class Move : MonoBehaviour
 
     }
 
+    void TurnOnMainEngine()
+    {
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(mainEngine);
+        }
+        if (!rocketJetParticle.isPlaying)
+        {
+            rocketJetParticle.Play();
+        }
+    }
+
+    void TurnOffMainEngine()
+    {
+        rocketJetParticle.Stop();
+        audioSource.Stop();
+    }
+
+    void TurnLeft()
+    {
+        ApplyRotation(rotationThrust);
+        if (!leftThrustleParticle.isPlaying)
+        {
+            leftThrustleParticle.Play();
+        }
+    }
+
+    void TurnRight()
+    {
+        ApplyRotation(-rotationThrust);
+        if (!rightThrustleParticle.isPlaying)
+        {
+            rightThrustleParticle.Play();
+        }
+    }
+
     void ApplyRotation(float rotationThisFrame)
     {
         rigidBody.freezeRotation = true;
         transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
         rigidBody.freezeRotation = false;
     }
+
+
+
 }
