025a9ce22b609af210324f3e6e3de5000665631c
VVIS <robertjwisniowski@gmail.com>
2021-07-23 19:46:30 +0200

    A NaN error caused by period ==0 has been removed.

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -19,13 +19,18 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float cycles = Time.time / period;  //continually groving over time
-        const float tau = Mathf.PI * 2; //constatnt value of 6.283
-        float rawSinWave = Mathf.Sin(cycles*tau);   //going from -1 to 1
+        if(period<=Mathf.Epsilon)
+        {
+            return;
+        }
+            float cycles = Time.time / period;  //continually groving over time
+            const float tau = Mathf.PI * 2; //constatnt value of 6.283
+            float rawSinWave = Mathf.Sin(cycles * tau);   //going from -1 to 1
 
-        movementFactor = (rawSinWave + 1f)/2f;  //recalculated to go from 0 to 1
+            movementFactor = (rawSinWave + 1f) / 2f;  //recalculated to go from 0 to 1
 
-        Vector3 offset = movementVector * movementFactor;
-        transform.position =startingPosition + offset;
+            Vector3 offset = movementVector * movementFactor;
+            transform.position = startingPosition + offset;
     }
 }
+ 
